0b11e738820fb9ec6b8e9a5eaea1f8e0bb589dac
Tashu-Gurnani2520 <tashugurnani2520@gmail.com>
2025-01-07 19:29:51 +0530

    commitfinal

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -14,7 +14,7 @@ function AboutSection() {
               What excites me the most is the potential for AI to revolutionize industries and enhance everyday life. I am committed to continuously learning, growing, and using my technical skills to make an impactful contribution to the future of technology.
             </p>
             <p className="mt-4 text-cyan-400 text-lg lg:text-xl font-bold">
-              Let’s connect! I’m open to job opportunities where I can contribute to creating the next generation of AI-driven solutions.
+              Lets connect! I am open to job opportunities where I can contribute to creating the next generation of AI-driven solutions.
             </p>
           </div>
         </div>
