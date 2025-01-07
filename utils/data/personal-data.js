e005e5e663eee31a63ca2d10c2e007d51ca280f4
Tashu-Gurnani2520 <tashugurnani2520@gmail.com>
2025-01-07 19:42:45 +0530

    commitfinal

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "TASHU GURNANI",
-  profile: '/profile.png',
+  profile: '',
   designation: "AI / ML Enthusiast",
   description: "What excites me the most is the potential for AI to revolutionize industries and enhance everyday life. I am committed to continuously learning, growing, and using my technical skills to make an impactful contribution to the future of technology. Let’s connect! I’m open to job opportunities where I can contribute to creating the next generation of AI-driven solutions.",
   email: 'tashugurnani2520@gmail.com',
